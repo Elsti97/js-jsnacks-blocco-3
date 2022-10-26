d4e477fb1eff097e920e8480b08d9776536c2341
Elsti97 <elsti.shehati@hotmail.it>
2022-10-26 15:58:09 +0200

    JSnack 3

--- main.js
+++ main.js
@@ -14,13 +14,31 @@
 
 
 // * JSnack 2
-let somma2 = 0;
+// let somma2 = 0;
+// let i = 0;
+
+// while(i < 5){
+//   let numero = parseInt(prompt("Inserisci un numero"));
+//   somma2 += numero;
+//   i++;
+// }
+
+// console.log(somma2);
+
+
+
+// * JSnack 3
+let N = parseInt(prompt("inserisci un numero per gli array"));
 let i = 0;
 
-while(i<5){
-  let numero = parseInt(prompt("Inserisci un numero"));
-  somma2 += numero;
+while(i < N){
+  let array3 = new Array();
+  let x = 0;
+  
+  while(x<10){
+    array3.push(Math.round(Math.random() * 100));
+    x++;
+  }
+  console.log(array3);
   i++;
 }
-
-console.log(somma2);
