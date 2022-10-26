3ed5752165478048fedc27954e8294207755ce57
Elsti97 <elsti.shehati@hotmail.it>
2022-10-26 14:52:10 +0200

    JSnack 2

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 // * wJsnack 1
-let array1 = [];
-let somma1 = 0;
+// let array1 = [];
+// let somma1 = 0;
 
-while(somma1 < 50) {
-  let numero = parseInt(prompt("Inserisci un numero < il 50"));
-  somma1 += numero;
-  array1.push(numero);
+// while(somma1 < 50) {
+//   let numero = parseInt(prompt("Inserisci un numero < il 50"));
+//   somma1 += numero;
+//   array1.push(numero);
+// }
+
+// console.log(array1);
+// console.log(somma1);
+
+
+
+// * JSnack 2
+let somma2 = 0;
+let i = 0;
+
+while(i<5){
+  let numero = parseInt(prompt("Inserisci un numero"));
+  somma2 += numero;
+  i++;
 }
 
-console.log(array1);
-console.log(somma1);
+console.log(somma2);
